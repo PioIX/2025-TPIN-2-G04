b7564ee58cfcca2d2263cbb291a288359017bafc
FelipeVilches8 <fvilches@pioix.edu.ar>
2025-11-11 16:55:36 -0300

    Remover comentario

--- front/src/app/home/page.js
+++ front/src/app/home/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-//Prueba pull request
+
 export default function HomePage() {
   const router = useRouter();
   const [user, setUser] = useState(null);
